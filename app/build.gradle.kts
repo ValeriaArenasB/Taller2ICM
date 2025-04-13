ef944f0ce807e0c3b7da467d386ee7fbcce0c540
Valeria Arenas <126576318+ValeriaArenasB@users.noreply.github.com>
2025-04-12 22:02:28 -0500

    LocationActivity added, bonus routes and #4 missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,13 +39,20 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.fragment:fragment-ktx:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
 }
